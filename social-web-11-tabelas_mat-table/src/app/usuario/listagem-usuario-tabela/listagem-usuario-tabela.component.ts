3a094bf3161b03300bcfd1d83270856e47d7a4bc
Gustavo Galisa <gustavogalisa@gmail.com>
2021-04-25 22:19:14 -0300

    updating

---
+++ social-web-11-tabelas_mat-table/src/app/usuario/listagem-usuario-tabela/listagem-usuario-tabela.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+import {Usuario} from '../../shared/model/usuario';
+import {UsuarioService} from '../../shared/services/usuario.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-listagem-usuario-tabela',
+  templateUrl: './listagem-usuario-tabela.component.html',
+  styleUrls: ['./listagem-usuario-tabela.component.scss']
+})
+export class ListagemUsuarioTabelaComponent implements OnInit {
+
+  dataSource: MatTableDataSource<Usuario>;
+  mostrarColunas = ['nome', 'cpf', 'idade', 'telefone', 'acoes'];
+
+  constructor(private usuarioService: UsuarioService, private roteador: Router) {
+  }
+
+  ngOnInit(): void {
+    this.usuarioService.listar().subscribe(
+      usuarios => this.dataSource = new MatTableDataSource(usuarios)
+    );
+  }
+
+  filtrar(texto: string): void {
+    this.dataSource.filter = texto.trim().toLowerCase();
+  }
+
+  editar(usuario: Usuario): void{
+    this.roteador.navigate(['cadastrarusuario', usuario.id]);
+  }
+
+  apagar(id: number): void {
+    this.usuarioService.remover(id).subscribe(
+      apagado => {
+        const indx = this.dataSource.data.findIndex(usuario => usuario.id === id);
+        if (indx > -1) {
+          this.dataSource.data.splice(indx, 1);
+          this.dataSource = new MatTableDataSource<Usuario>(this.dataSource.data);
+        }
+      }
+    );
+  }
+}
